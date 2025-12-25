e8b3413eef05c0cc215d3d79ea22a5a92737839e
Hani <hanider27@gmail.com>
2025-12-25 17:01:26 +0100

    Remove public folder from git

--- public/js/checkauth.js
+++
@@ -1,9 +0,0 @@
-async function checkAuth() {
-    const response = await fetch("/users", {
-        credentials: "include"
-    });
-
-    if (!response.ok) {
-        window.location.href = "login.html";
-    }
-}
